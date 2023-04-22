68bc3273720fe734c35a67830aff270177f86123
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-22 11:45:37 -0400

    initial commit

---
+++ testing.rb
@@ -0,0 +1,19 @@
+puts "Hello World" # console.log in javascript
+
+# Data Types
+myint = 5 
+mystr = "Hello World"
+myfloat = 65.76
+mybool = true
+
+puts(myint, mystr, myfloat, mybool)
+
+# Conditionals
+myvar = 5 > 6 # expression
+puts(myvar)
+
+if myvar
+    puts('This is true')
+else
+    puts('This is false')
+end
